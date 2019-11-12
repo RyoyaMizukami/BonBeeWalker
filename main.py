2e3611b8145f581daaa18a0f0cd311dd70f51e56
RyoyaMizukami <s18102@kaetsu.ac.jp>
2019-11-12 21:16:05 +0900

    登録フォーム調整用

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for, escape, re
 import psycopg2
 import os
 app = Flask(__name__)
+API_key = os.environ.get('gmap_API')
 
 def get_connection():
     dsn = os.environ.get('DATABASE_URL')
@@ -28,46 +29,61 @@ def search():
     print(menu)
     return render_template('commons/search2.html', menu = menu, IsValue = IsValue)
 
-@app.route('/result')
-
+@app.route('/resister')
+def resister():
+    return render_template('commons/resister.html', API_key = API_key)
 
 @app.route('/search2')
 def search2():
     IsValue = False
     return render_template('commons/search2.html', IsValue = IsValue)
 
-@app.route('/post', methods=['GET', 'POST'])
+@app.route('/post', methods=['POST'])
 def post():
     IsValue = False
-    if request.method == 'POST':
 
-        budget = request.form['budget']
-        type = request.form['type']
-        print(budget)
-        print(type)
-        if not type and not budget:
-            IsValue = True
-            return render_template('commons/search2.html', IsValue = IsValue)
+    budget = request.form['budget']
+    type = request.form['type']
+    print(budget)
+    print(type)
+    if not type and not budget:
+        IsValue = True
+        return render_template('commons/search2.html', IsValue = IsValue)
 
-        cur = get_connection().cursor()
+    cur = get_connection().cursor()
 
-        if type and not budget:
-            cur.execute('SELECT type,money,place,cando FROM zgundam WHERE type LIKE %s ORDER BY money ASC', (type,))
-            data = cur.fetchall()
-            cur.close()
-            get_connection().close()
-        elif budget and not type:
-            cur.execute('SELECT type,money,place,cando FROM zgundam WHERE money <= %s ORDER BY money ASC', (budget,))
-            data = cur.fetchall()
-            cur.close()
-            get_connection().close()
-        else:
-            cur.execute('SELECT type,money,place,cando FROM zgundam WHERE money <= %s AND type LIKE %s ORDER BY money ASC', (budget,type,))
-            data = cur.fetchall()
-            cur.close()
-            get_connection().close()
+    if type and not budget:
+        cur.execute('SELECT type,money,place,cando FROM zgundam WHERE type LIKE %s ORDER BY money ASC', (type,))
+        data = cur.fetchall()
+        cur.close()
+        get_connection().close()
+    elif budget and not type:
+        cur.execute('SELECT type,money,place,cando FROM zgundam WHERE money <= %s ORDER BY money ASC', (budget,))
+        data = cur.fetchall()
+        cur.close()
+        get_connection().close()
+    else:
+        cur.execute('SELECT type,money,place,cando FROM zgundam WHERE money <= %s AND type LIKE %s ORDER BY money ASC', (budget,type,))
+        data = cur.fetchall()
+        cur.close()
+        get_connection().close()
 
-        return render_template('index.html', budget = budget, type = type, data = data, len = len(data))
+    return render_template('index.html', budget = budget, type = type, data = data, len = len(data))
+
+@app.route('/resister_post', methods=['POST'])
+def registing():
+    type = request.form['type']
+    money = request.form['money']
+    placeID = request.form['placeID']
+    cando = request.form['cando']
+
+    if type and money and placeID and cando:
+        print(type, money, placeID, cando)
+        IsValue = False
+        return render_template('commons/search2.html', IsValue = IsValue)
+    else:
+        IsValue = True
+        return render_template('commons/resister.html', IsValue = IsValue)
 
 if __name__ == "__main__":
     app.run()
--- test/test.py
+++ test/test.py
@@ -5,9 +5,20 @@ import os
 app = Flask(__name__)
 
 API_key = os.environ.get('gmap_API')
+print(API_key)
 
 @app.route('/')
 def index():
+    return render_template('commons/resister.html', API_key = API_key)
+
+#@app.route('/resister')
+#def resister():
+#    return render_template('commons/resister.html', API_key = API_key)
+
+@app.route('/post', methods=['POST'])
+def post():
+    place_id = request.form['placeID']
+    print(place_id)
     return render_template('commons/api_test.html', API_key = API_key)
 
 if __name__ == "__main__":
